95d9b6123089a24335c6a414b42046ba72e9630a
JERONIMORAMIREZRESTREPO <jeronimoramirez1159288@correo.itm.edu.co>
2026-03-05 20:11:29 -0500

    program to understand stacks using an array

---
+++ StacksAndQueuesSolution/SAQ.Backend/StackUsingArray.cs
@@ -0,0 +1,40 @@
+﻿namespace SAQ.Backend;
+
+public class StackUsingArray<T>
+{
+    private T[] _stack;
+    private int _top;
+
+    public StackUsingArray(int capacity)
+    {
+        _stack = new T[capacity];
+        _top = -1;
+    }
+
+    public void Push(T item)
+    {
+        if (_top == _stack.Length - 1)
+        {
+            throw new InvalidOperationException("Stack is full.");
+        }
+        _stack[++_top] = item; //increment the top index and then add the item to the stack
+    }
+
+    public T Pop()
+    {
+        if (_top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        return _stack[_top--]; //return the top item and then decrement the top index
+    }
+
+    public T Peek()
+    {
+        if (_top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        return _stack[_top]; //return the top item without modifying the top index
+    }
+}
